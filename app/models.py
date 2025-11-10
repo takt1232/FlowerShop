d8ec3a2b4761c770aa4bec75714ba8a35d038806
Jericho <133334830+takt1232@users.noreply.github.com>
2025-11-10 23:54:09 +0800

    Revert "initial setup"
    This reverts commit d5425364b75b387ec72cfb7fa6f524563846253f.

--- app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Flower(models.Model):
-    name = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='flowers/', null=True, blank=True)
-
-    def __str__(self):
-        return self.name

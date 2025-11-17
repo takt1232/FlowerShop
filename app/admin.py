2f29543496bd1eadeb837f42392efe4cd1c9f5e4
Jericho <133334830+takt1232@users.noreply.github.com>
2025-11-17 21:04:22 +0800

    added flower category

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Flower
+from .models import Flower, FlowerCategory
 
 @admin.register(Flower)
 class FlowerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'image')
+    search_fields = (['name', 'category'])
+    
+@admin.register(FlowerCategory)
+class FlowerCategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'image')
-    search_fields = ('name',)
+    search_fields = (['name'])
     
--- app/models.py
+++ app/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+
+class FlowerCategory(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='flowers')
+
+    def __str__(self):
+        return self.name
+    
 class Flower(models.Model):
     name = models.CharField(max_length=255)
+    category = models.ForeignKey(FlowerCategory, on_delete=models.SET_NULL, null=True, blank=True)
     image = models.ImageField(upload_to='flowers/', null=True, blank=True)
 
     def __str__(self):
